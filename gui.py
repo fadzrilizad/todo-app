b4cb3031a30be256fd8d382b844db0ee2565d969
fadzrilizad <fadzrilizad93@gmail.com>
2023-09-19 10:11:37 +0800

    update gui

--- gui.py
+++ gui.py
@@ -4,10 +4,16 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a todo: ")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='todos', enable_events=True,
+                      size=(45, 10))
+
+edit_button = sg.Button("Edit")
 
 structure = [
                 [label],
-                [input_box, add_button]
+                [input_box, add_button],
+                [list_box, edit_button]
             ]
 
 font_size = ("Helvetica", 12)
